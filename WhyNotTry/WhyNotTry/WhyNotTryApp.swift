5996122fbb05e51cc10cd1d8283591cef524071a
Wisam <wisam@windfallapps.com>
2024-01-29 12:53:56 -0800

    WhyNotTry

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Wisam Alhamad on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
